c85dabe64893ebfb392e6600eed56aa6c3c7eae4
olamilekan <olamilekanomosanya@gmail.com>
2022-12-20 21:26:21 +0100

    add:mods

--- js/testimonial.js
+++ js/testimonial.js
@@ -1,13 +1,38 @@
-$(document).ready(function () {
-  $("#testimonial-slider").owlCarousel({
-    items: 2,
-    itemsDesktop: [1000, 2],
-    itemsDesktopSmall: [979, 2],
-    itemsTablet: [768, 2],
-    itemsMobile: [650, 1],
-    pagination: true,
-    navigation: false,
-    slideSpeed: 1000,
-    autoPlay: true,
-  });
+$(document).ready(function(){
+    
+  $('.items').slick({
+dots: true,
+infinite: true,
+speed: 800,
+autoplay: true,
+autoplaySpeed: 2000,
+slidesToShow: 4,
+slidesToScroll: 4,
+responsive: [
+  {
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      infinite: true,
+      dots: true
+    }
+  },
+  {
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 2
+    }
+  },
+  {
+    breakpoint: 480,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  }
+
+]
 });
+        });
